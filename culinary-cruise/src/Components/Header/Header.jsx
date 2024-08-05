a479e6061513107f169a1973556ff42060cb6bc1
Karan <kc2532002@gmail.com>
2024-08-06 03:56:28 +0530

    made the website responsive

--- culinary-cruise/src/Components/Header/Header.jsx
+++ culinary-cruise/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Place your order for your favorite food here.</h2>
+        <h2>Order your favorite food here.</h2>
         <p>We offer a wide range of options to suit every taste and preference. Whether you crave something sweet, savory, or refreshing, you'll find the perfect choice. Enjoy a delightful experience tailored just for you. Start your order now and indulge in your favorites!</p>
         <button>View Menu</button>
       </div>

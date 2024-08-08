074292a082090201ef943bd3d02e3d5e94d8144d
Karan <kc2532002@gmail.com>
2024-08-09 02:27:52 +0530

    cart functionality

--- culinary-cruise/src/Pages/Cart/Cart.jsx
+++ culinary-cruise/src/Pages/Cart/Cart.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import {StoreContext} from "../../context/StoreContext"
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
   // adding the cart functionality
-  const {food_list,cartItems,addToCart,setCartItems,removeFromCart} = useContext(StoreContext);
+  const { food_list, cartItems, addToCart, setCartItems, removeFromCart } =
+    useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -19,29 +20,30 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index) => {
+        {food_list.map((item, index) => {
           //this logic : when we'll add the items to the cart, it will show here
-            if(cartItems[item._id] > 0){
-              return(
-                <div className="cart-items-title cart-items">
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
                   <img src={item.image} />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>₹ {item.price}</p>
+                  <p>{cartItems[item._id]}</p>{" "}
                   {/* // this will show quatity of the items selected */}
-                  <p>{cartItems[item._id]}</p> 
-                  <p>{console.log(cartItems[item._id])}</p>
-                  {/* to get the price we'll be multiplying Quantity x price */}
+                  {/* <p>{console.log(cartItems[item._id])}</p> */}
                   <p>₹ {item.price * cartItems[item._id]}</p>
-                  <p>x</p>
+                  {/* to get the price we'll be multiplying Quantity x price */}
+                  <p onClick={() => removeFromCart(item._id)}className="cross">x</p>
                 </div>
-              )
-            }
-        })
-        }
+                <hr />
+              </div>
+            );
+          }
+        })}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
